bbfca45cbe0922c089b81ca855bcfb96a7ff060b
Igor Vasilevich <vasilevich.igor@gmail.com>
2026-08-28 16:32:56 +0300

    final

--- keyboards/keyboards.py
+++ keyboards/keyboards.py
@@ -1,4 +1,4 @@
-from aiogram import KeyboardButton, ReplyKeyboardMarkup
+from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
 from aiogram.utils.keyboard import ReplyKeyboardBuilder
 from lexicon.lexicon import LEXICON_RU
 
